bb63f13d1bde8f458cb524463e9af6c7021642f7
Gary Patricelli <garypatricelli@gmail.com>
2014-03-21 23:07:31 -0400

    add data type for spaces

--- dust_sweeper.hs
+++ dust_sweeper.hs
@@ -3,7 +3,13 @@
 -- Date: 03/21/2014
 -- Written By: Gary Patricelli                      
 
+-- Define data type for a space
+data Space = Space { hasDust :: Bool
+                   , isDiscovered :: Bool
+                   , numAdjacent :: Int
+                   } deriving (Show)
  
+-- Main method, mostly test code at the moment...
 main = do
     putStrLn "Welcome to dust sweeper!"
     putStrLn "Please enter the board size."
@@ -12,10 +18,10 @@ main = do
     let gameBoard = board (read boardSize :: Int) '#'
     printBoard gameBoard
 
--- Create a 2d list of size 'x' filled with 'a'
-board :: Int -> char -> [[char]]
+-- Create a 2D list of size 'x' filled with 'a'
+board :: Int -> a -> [[a]]
 board x = replicate x . replicate x
 
 -- Print the game board
-printBoard :: [[char]] -> [String]
+printBoard :: [[a]] -> [String]
 printBoard x = [show a | y <- x, a <- y] 
