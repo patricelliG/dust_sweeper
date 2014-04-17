5e4d0bbdfb5ec588adde9641ab4de4109b2e05d1
Gary Patricelli <garypatricelli@gmail.com>
2014-04-17 10:41:52 -0400

    add print board for Integers

--- dust_sweeper.hs
+++ dust_sweeper.hs
@@ -10,18 +10,31 @@ data Space = Space { hasDust :: Bool
                    } deriving (Show)
  
 -- Main method, mostly test code at the moment...
-main = do
-    putStrLn "Welcome to dust sweeper!"
-    putStrLn "Please enter the board size."
-    boardSize <- getLine
-    putStrLn ("Creating game board of size " ++ boardSize)
-    let gameBoard = board (read boardSize :: Int) '#'
-    printBoard gameBoard
+-- main = do
+--    putStrLn "Welcome to dust sweeper!"
+--    putStrLn "Please enter the board size."
+--    boardSize <- getLine
+--    putStrLn ("Creating game board of size " ++ boardSize)
+--    let gameBoard = board (read boardSize :: Int) '#'
+--    printBoard gameBoard
 
 -- Create a 2D list of size 'x' filled with 'a'
 board :: Int -> a -> [[a]]
 board x = replicate x . replicate x
 
+-- Conversion chart for spaces 
+value 0 = 'x'
+value 1 = 'a'
+
+-- Convert to user view
+convert :: [[Integer]] -> [String]
+convert = map(map value)
+
+-- Print map
+printMap :: [[Integer]] -> IO()
+printMap = putStrLn . unlines . convert
+
+
 -- Print the game board
-printBoard :: [[a]] -> [String]
-printBoard x = [show a | y <- x, a <- y] 
+--printBoard :: [[a]] -> [String]
+--printBoard x = [show a | y <- x, a <- y] 
