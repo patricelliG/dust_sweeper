94a44af4d41c96f6927e735a9d808ec092e2a63b
Gary Patricelli <garypatricelli@gmail.com>
2014-04-17 17:44:41 -0400

    add function to change a space

--- dust_sweeper.hs
+++ dust_sweeper.hs
@@ -3,6 +3,17 @@
 -- Date: 03/21/2014
 -- Written By: Gary Patricelli                      
 
+-- Notes
+-- Must install cabal packages for random "cabal install cabal-install"
+
+-- TODO
+-- Add bombs at random
+-- Move on a space
+-- Calculate adjBombs
+
+import System.Random -- Requires cabal
+import Data.List
+
 -- Define data type for a space
 data Space = Space { hasDust :: Bool
                    , isDiscovered :: Bool
@@ -18,7 +29,7 @@ value :: Space -> Char
 value (Space dust discovered adjBombs) 
     | discovered /= True = 'X'
     | discovered == True && dust /= True = head $ show adjBombs
-    | otherwise = 'F'
+    | otherwise = 'D'
 
 -- Return board with with visible characters 
 convert :: [[Space]] -> [[Char]]
@@ -28,6 +39,18 @@ convert = map(map value)
 printBoard :: [[Space]] -> IO()
 printBoard = putStrLn . unlines . convert
 
+-- Return a board with elment at "row", "column" replaced with "newSpace" 
+changeSpace :: [[Space]] -> Int -> Int -> Space -> [[Space]]
+changeSpace board row column newSpace
+    | row > length board || column > length board = board 
+    | otherwise = case splitAt column (board!!row) of
+        (front, oldSpace:tail) -> restoreBoard board (front ++ newSpace : tail) row
+
+-- Recombine broken down 2D list, helper function for changeSpace
+restoreBoard :: [[Space]] -> [Space] -> Int -> [[Space]] 
+restoreBoard board newRow splitRow
+    | otherwise = case splitAt splitRow board of
+        (top, oldRow:bottom) -> top ++ newRow : bottom
 
 
 
